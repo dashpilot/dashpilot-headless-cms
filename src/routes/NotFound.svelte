41e40abe7ca855d9e41e68db1947ead7441e154a
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-01-20 23:01:19 +0000

    Improved the 404

---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,16 @@
+
+<div class="row topnav">
+<div class="col-6">
+<h4>Error 404</h4>
+</div>
+<div class="col-6 text-right">
+
+</div>
+</div>
+
+<div class="content">
+	
+	Page not found.
+	
+</div>
+
